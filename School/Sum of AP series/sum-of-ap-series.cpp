6e7ae7f0f72f87414a8c1292cf106096cbd41070
Mohana <97424463+21A91A05E6@users.noreply.github.com>
2024-06-17 10:12:40 +0530

    Added Solution - GfG to GitHub

---
+++ School/Sum of AP series/sum-of-ap-series.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+	public:
+		long sum_of_ap(long n,long a, long d)
+		{
+		    // Code here.
+		    //Sn = n/2 [2a + (n-1)d]
+		    long s;
+		    s=(n * (2 * a + (n - 1) * d)) / 2;
+		    return s;
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	long n, a, d;
+    	cin >> n >> a >> d;
+    	Solution ob;
+    	long ans = ob.sum_of_ap(n, a, d);
+    	cout << ans <<"\n";
+    }
+	return 0;
+}
+// } Driver Code Ends
