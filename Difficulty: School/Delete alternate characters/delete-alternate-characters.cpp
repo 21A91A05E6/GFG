779864ae015646568db6790a3ae301c91659f8d1
Mohana <97424463+21A91A05E6@users.noreply.github.com>
2024-06-19 19:36:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Delete alternate characters/delete-alternate-characters.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    string delAlternate(string s) {
+        // code here
+        int n=s.size();
+        string a;
+        for(int i=0;i<n;i+=2){
+            a+=s[i];
+        }
+        return a;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.delAlternate(S) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
